82bf9334f559c49f26f9d63b212634f7667440cc
Kevin Gentile <gentile_kevin94@hotmail.com>
2017-10-10 22:08:59 -0400

    Fix directory consistent with documentation

--- main.go
+++ main.go
@@ -12,10 +12,14 @@ import (
 func main() {
 	fmt.Println("Web Start")
 	router := mux.NewRouter() // Create gorilla router
+
+	// "http://kevingentile.com/index.html"
 	router.HandleFunc("/", makeHandler(IndexHandler))
+	// "http://kevingentile.com/assets/*"
 	router.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir("assets")))) //TODO this seems excessive...
+	// "http://kevingentile.com/images/*"
 	router.PathPrefix("/images/").Handler(http.StripPrefix("/images/", http.FileServer(http.Dir("images"))))
 
-	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router)) // Listen on port defined by env variable PORT
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router)) // Listen on port defined by environment variable PORT
 
 }
