d03df72d4aef8543c034e86cc9322eadb884d226
Kevin Gentile <gentile_kevin94@hotmail.com>
2017-10-19 14:45:19 -0400

    Make index dynamically typed

--- indexHandler.go
+++ indexHandler.go
@@ -7,19 +7,36 @@ import (
 )
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	//type ReelArticle struct {
+	//	Title     string
+	//	Body      string
+	//	ImageLink string
+	//	PicPath   string
+	//}
 	carousel := web.Carousel{
 		Articles: []web.ReelArticle{
-			{Title: "TEST ARTICLE", ImageLink: "#", Body: "test body"},
+			{Title: "kevingentile.com",
+				Body:      "This website! Built using Go and the HTML5UP site template.",
+				ImageLink: "https://github.com/LaughingCabbage/kevingentile.com",
+				PicPath:   "../images/pic01.jpg",
+			},
+			{Title: "Image Text Processing",
+				Body:      "Find and draw a box around letters in an image.",
+				ImageLink: "https://github.com/LaughingCabbage/isoChar",
+				PicPath:   "../images/pic02.jpg",
+			},
+			{Title: "Arcade Game",
+				Body:      "A game built with the SFML framework in C++",
+				ImageLink: "https://github.com/LaughingCabbage/Asteroid",
+				PicPath:   "../images/pic03.jpg",
+			},
+			{Title: "Blockchain",
+				Body:      "Simple blockchain built in Go to better understand cryptocurrency",
+				ImageLink: "https://github.com/LaughingCabbage/goLinks",
+				PicPath:   "../images/pic04.jpg",
+			},
 		},
 	}
-	/*
-		tmpl := "index.template.html"                       //add html tag
-		err := templates.ExecuteTemplate(w, tmpl, carousel) //attempt to render the template
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError) //Template not found/ not rendered
-		}
-	*/
-	renderTemplate(w, "index", Data{
-		"carousel": carousel,
-	})
+	data := Data{"carousel": carousel}
+	renderTemplate(w, "index", data)
 }
