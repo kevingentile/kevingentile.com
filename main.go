ff029e5c7ce006b0f2988f79da7b699b67280cfb
Kevin Gentile <kgentile@protonmail.com>
2018-05-29 19:32:09 -0400

    fix port bug

--- main.go
+++ main.go
@@ -27,6 +27,6 @@ func main() {
 	router.PathPrefix("/images/").Handler(http.StripPrefix("/images/", http.FileServer(http.Dir("images"))))
 
 	log.Println(templates.DefinedTemplates())
-	log.Fatal(http.ListenAndServe(":8080", router)) // Listen on port defined by environment variable PORT
+	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), router)) // Listen on port defined by environment variable PORT
 
 }
