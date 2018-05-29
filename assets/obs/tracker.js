f3ffa76ecde95416c28ef718975a8cf5fddfd963
Kevin Gentile <kgentile@protonmail.com>
2018-05-28 23:27:44 -0400

    fix interval bug

--- assets/obs/tracker.js
+++ assets/obs/tracker.js
@@ -1,29 +1,30 @@
 var app = new Vue({
     el: '#app',
     data: {
-        timeout: 3 * 60 * 60,
+        interval: 60000,
         stats: {
             kills: 0,
             wins: 0,
             kdr: 0.0
         }
     },
-    
     created: function(){
         this.start();
     },
     methods:{
         start: function(){
-            this.getData();
-            setTimeout(function(){
-               app.getData();
-            }, this.timeout);
+            var self = this;
+            self.getData();
+            setInterval(function(){
+                self.getData();
+            }, this.interval);
         },
         getData: function(){
+            var self = this;
             axios.get(`/obs/laughingcabbage`)
             .then(response => {
-                this.stats = response.data
-            })
+                self.stats = response.data;
+            });
         }
     }
-})
+});
