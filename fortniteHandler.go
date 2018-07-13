c10b8cf455cb9dc2e8db432990e4f273cdd45ac6
Kevin Gentile <kgentile@protonmail.com>
2018-07-12 20:58:06 -0400

    actually handle a bad request

--- fortniteHandler.go
+++ fortniteHandler.go
@@ -20,28 +20,33 @@ func handleFortniteData(w http.ResponseWriter, r *http.Request) {
 	key := os.Getenv("KEY")
 	profile, err := tracker.GetProfile("pc", "laughingcabbage", key)
 	if err != nil {
-		panic(err)
+		handleError(err, w)
 	}
 	data := Data{}
 
 	kills, err := tracker.GetKills(profile)
-	handleError(err)
+	handleError(err, w)
 	data.Kills = kills
 
 	wins, err := tracker.GetWins(profile)
-	handleError(err)
+	handleError(err, w)
 	data.Wins = wins
 
 	kdr, err := tracker.GetCurrentKDR(profile)
-	handleError(err)
+	handleError(err, w)
 	data.KDR = kdr
 
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(data)
 }
 
-func handleError(err error) {
+func handleError(err error, w http.ResponseWriter) {
 	if err != nil {
-		panic(err)
+		data := Data{
+			Wins:  -1,
+			KDR:   -10.0,
+			Kills: 2,
+		}
+		json.NewEncoder(w).Encode(data)
 	}
 }
