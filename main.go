eb914e7bf57141690b572d43a36dde4ca0e3536c
Kevin Gentile <kevin@kevingentile.com>
2019-02-22 20:42:45 -0500

    add rate limiter

--- main.go
+++ main.go
@@ -4,13 +4,18 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	"github.com/gorilla/mux"
 )
 
+var limiter <-chan time.Time
+
 func main() {
 	log.Println("keivngentile.com Web Start PORT: " + os.Getenv("PORT"))
 	router := mux.NewRouter() // Create gorilla router
+
+	limiter = time.Tick(time.Second * 3)
 
 	// "http://kevingentile.com/index.html"
 	router.HandleFunc("/", makeHandler(IndexHandler))
@@ -20,7 +25,7 @@ func main() {
 	// "http://kevingentile.com/links.html"
 	router.HandleFunc("/links.html", makeHandler(LinksHandler))
 	// "http://kevingentile.com/obs/laughingcabbage" JSON // TODO rate limit this handler
-	router.HandleFunc("/obs/laughingcabbage", handleFortniteData)
+	router.HandleFunc("/obs/laughingcabbage", rateLimit(handleFortniteData, &limiter))
 	// "http://kevingentile.com/assets/*"
 	router.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", http.FileServer(http.Dir("assets"))))
 	// "http://kevingentile.com/images/*"
