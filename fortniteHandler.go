f3e3f0a4bea43eef2f9caedc7ea103c0fee65c4c
Kevin Gentile <kgentile@protonmail.com>
2020-10-01 17:48:44 -0400

    simplify, refactor rate limiter, remove unused

--- fortniteHandler.go
+++ fortniteHandler.go
@@ -4,9 +4,9 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
-	"os"
 
-	"github.com/gorilla/mux"
+	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 
 	tracker "github.com/kevingentile/fortnite-tracker"
 )
@@ -18,43 +18,39 @@ type Data struct {
 	Kills int     `json:"kills"`
 }
 
-func handleFortniteData(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	key := os.Getenv("KEY")
-	profile, err := tracker.GetProfile(vars["platform"], vars["username"], key)
+func handleFortniteData(c *gin.Context) {
+	platform := c.Param("platform")
+	username := c.Param("username")
+	key := viper.GetString("fortnite_tracker_token")
+	profile, err := tracker.GetProfile(platform, username, key)
 	if err != nil {
-		handleError(err, w)
+		c.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
 	data := Data{}
 
 	kills, err := profile.GetKills()
 	if err != nil {
-		handleError(err, w)
+		c.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
 	data.Kills = kills
 
 	wins, err := profile.GetWins()
 	if err != nil {
-		handleError(err, w)
+		c.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
 	data.Wins = wins
 
 	kdr, err := profile.GetCurrentKDR()
 	if err != nil {
-		handleError(err, w)
+		c.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
 	data.KDR = kdr
 
-	w.Header().Set("Content-Type", "application/json")
-	err = json.NewEncoder(w).Encode(data)
-	if err != nil {
-		handleError(err, w)
-		return
-	}
+	c.JSON(http.StatusOK, data)
 }
 
 func handleError(err error, w http.ResponseWriter) {
