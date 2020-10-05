e1bc9588c605d606ab7a246b482ad4f1b41eab87
Kevin Gentile <kgentile@protonmail.com>
2020-10-05 14:35:47 -0400

    fix https upgrade for obfuscated request protocol

--- httpsUpgradeHandler.go
+++ httpsUpgradeHandler.go
@@ -10,12 +10,10 @@ import (
 
 func UpgradeHTTPSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		scheme := c.Request.URL.Scheme
-		if scheme == "http" || (scheme == "" && !viper.GetBool("development")) {
+		if !viper.GetBool("development") && c.Request.Header.Get("x-forwarded-proto") != "https" {
 			targetURL := url.URL{
 				Scheme: "https", Host: c.Request.Host, Path: c.Request.URL.Path, RawQuery: c.Request.URL.RawQuery,
 			}
-			Debug("HTTPS upgrade source:", c.Request.URL.String(), "| target: ", targetURL.String())
 			c.Redirect(http.StatusTemporaryRedirect, targetURL.String())
 			c.Abort()
 			return
