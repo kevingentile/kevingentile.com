af73656d039a83ed2acc9dcb6f9860ab1369e9f9
Kevin Gentile <kgentile@protonmail.com>
2020-10-05 12:35:51 -0400

    fix https upgrade

--- httpsMiddleware.go
+++ httpsMiddleware.go
@@ -2,16 +2,21 @@ package main
 
 import (
 	"net/http"
+	"net/url"
 
 	"github.com/gin-gonic/gin"
+	"github.com/spf13/viper"
 )
 
 func UpgradeHTTPS() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		url := c.Request.URL
-		if url.Scheme == "http" {
-			url.Scheme = "https"
-			c.Redirect(http.StatusPermanentRedirect, url.String())
+		scheme := c.Request.URL.Scheme
+		if scheme == "http" || (!viper.GetBool("development") && scheme == "") {
+			targetURL := url.URL{
+				Scheme: "https", Host: c.Request.Host, Path: c.Request.URL.Path, RawQuery: c.Request.URL.RawQuery,
+			}
+			c.Redirect(http.StatusTemporaryRedirect, targetURL.String())
+			c.Abort()
 			return
 		}
 
